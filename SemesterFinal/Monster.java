3eba2843fe5f3cc7a0e6de517ad8337994c78614
Griffin Holloway <GriffinHolloway@mcsd.murrayschools.org>
2016-05-16 12:49:16 -0600

    Monster class stats being added

--- SemesterFinal/Monster.java
+++ SemesterFinal/Monster.java
@@ -7,13 +7,19 @@
  */
 public class Monster
 {
-    public void createMonsters()
+    public void createMonsters(int h, int d, int a)
     {
-        Monster giantCaveSpider = new Monster();
-        Monster troll = new Monster();
+        Monster giantCaveSpider = new Monster(h=32,d=4,a=6);
+        Monster troll = new Monster(27,5,8);
         Monster batHybrid = new Monster();
         Monster giantKingKobra = new Monster();
         Monster giantScorpion = new Monster();
         Monster skeletonPirateFinal = new Monster();
     }
+    
+    public void getStats()
+    {
+        
+    }
+    
 }
