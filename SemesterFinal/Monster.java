674a0676f33a105059ee722c80a02165d401a729
Griffin Holloway <griffin2@comcast.net>
2016-05-20 12:55:18 -0600

    Monsters are still here

--- SemesterFinal/Monster.java
+++ SemesterFinal/Monster.java
@@ -220,16 +220,16 @@ public class Monster
                         strength = 5;
                         defense = 5;
                         speed = 3;
-                        name="Garrett";
+                        name="Blake";
                         niceHair=true;
                     }
                     else if(monsters==1)
                     {
                         health=27;
                         strength = 7;
-                        defense = 3;
-                        speed = 1;
-                        name="Orc";
+                        defense = 6;
+                        speed = 5;
+                        name="Grey Wolf";
                         niceHair=true;
                     }
                     else
@@ -242,19 +242,248 @@ public class Monster
 
                     if(monsters==0)
                     {
-                        health=7;
+                        health=32;
+                        strength = 8;
+                        defense = 5;
+                        speed = 5;
+                        name="Undead Foot Soldier";
+                        niceHair=true;
+                    }
+                    else if(monsters==1)
+                    {
+                        health=28;
+                        strength = 9;
+                        defense = 8;
+                        speed = 5;
+                        name="Hell GateKeeper";
+                        niceHair=true;
+                    }
+                    else
+                        System.out.println("Error: Not a Valid Monster");
+                }while(!niceHair);
+            }
+            else if(kind==10)
+            {
+                do{
+                    if(monsters==0)
+                    {
+                        health = 1;
+                        strength =2 ;
+                        defense = 1;
+                        speed = 1;
+                        name = "Spider";
+                        niceHair=true;
+                    }
+                    else if(monsters==1)
+                    {
+                        health=2;
+                        strength = 1;
+                        defense = 1;
+                        speed = 1;
+                        name="Scorpion";
+                        niceHair=true;
+                    } 
+                    else
+                        System.out.println("Error: Not a Valid Monster");
+                }while(!niceHair);
+            }
+            else if(kind==11)
+            {
+                do{
+                    if(monsters==0)
+                    {
+                        health=3;
+                        strength = 3;
+                        defense = 2;
+                        speed = 2;
+                        name="Troll";
+                        niceHair=true;
+                    }
+                    else if(monsters==1)
+                    {
+                        health=4;
+                        strength = 2;
+                        defense = 3;
+                        speed =1;
+                        name="Giant";
+                        niceHair=true;
+                    }
+                    else
+                        System.out.println("Error: Not a Valid Monster");
+                }while(!niceHair);
+            }
+            else if(kind==12)
+            {
+                do{
+                    if(monsters==0)
+                    {
+                        health=5;
+                        strength=5;
+                        defense=2;
+                        speed=3;
+                        name="Skeleton Pirate";
+                        niceHair=true;
+                    }
+                    if(monsters==1)
+                    {
+                        health=8;
+                        strength=2;
+                        defense=3;
+                        speed=2;
+                        name="Apperition";
+                        niceHair=true;
+                    }
+                    else
+                        System.out.println("Error: Not a Valid Monster");
+                }while(!niceHair);
+            }
+            else if(kind==13)
+            {
+                do{
+
+                    if(monsters==0)
+                    {
+                        health=10;
+                        strength = 5;
+                        defense = 2;
+                        speed = 3;
+                        name="Garrett";
+                        niceHair=true;
+                    }
+                    else if(monsters==1)
+                    {
+                        health=8;
+                        strength = 7;
+                        defense = 3;
+                        speed = 1;
+                        name="Orc";
+                        niceHair=true;
+                    }
+                    else
+                        System.out.println("Error: Not a Valid Monster");
+                }while(!niceHair);
+            }
+            else if(kind==14)
+            {
+                do{
+
+                    if(monsters==0)
+                    {
+                        health=13;
                         strength = 7;
                         defense = 8;
-                        speed = 3;
+                        speed = 2;
                         name="Undead Foot Soldier";
                         niceHair=true;
                     }
                     else if(monsters==1)
                     {
                         health=12;
-                        strength = 9;
+                        strength = 8;
                         defense = 4;
-                        speed = 7;
+                        speed = 6;
+                        name="Hell GateKeeper";
+                        niceHair=true;
+                    }
+                    else
+                        System.out.println("Error: Not a Valid Monster");
+                }while(!niceHair);
+            }
+             else if(kind==15)
+            {
+                do{
+                    if(monsters==0)
+                    {
+                        health=25;
+                        strength = 5;
+                        defense = 3;
+                        speed = 2;
+                        name="Vampire";
+                        niceHair=true;
+                    }
+                    else if(monsters==1)
+                    {
+                        health=22;
+                        strength = 8;
+                        defense = 3;
+                        speed =2;
+                        name="Werewolf";
+                        niceHair=true;
+                    }
+                    else
+                        System.out.println("Error: Not a Valid Monster");
+                }while(!niceHair);
+            }
+            else if(kind==16)
+            {
+                do{
+                    if(monsters==0)
+                    {
+                        health=30;
+                        strength=5;
+                        defense=2;
+                        speed=3;
+                        name="Golem";
+                        niceHair=true;
+                    }
+                    if(monsters==1)
+                    {
+                        health=25;
+                        strength=7;
+                        defense=6;
+                        speed=2;
+                        name="Cave Person";
+                        niceHair=true;
+                    }
+                    else
+                        System.out.println("Error: Not a Valid Monster");
+                }while(!niceHair);
+            }
+            else if(kind==17)
+            {
+                do{
+
+                    if(monsters==0)
+                    {
+                        health=32;
+                        strength = 5;
+                        defense = 5;
+                        speed = 3;
+                        name="Blake";
+                        niceHair=true;
+                    }
+                    else if(monsters==1)
+                    {
+                        health=27;
+                        strength = 7;
+                        defense = 6;
+                        speed = 5;
+                        name="Grey Wolf";
+                        niceHair=true;
+                    }
+                    else
+                        System.out.println("Error: Not a Valid Monster");
+                }while(!niceHair);
+            }
+            else if(kind==18)
+            {
+                do{
+
+                    if(monsters==0)
+                    {
+                        health=32;
+                        strength = 8;
+                        defense = 5;
+                        speed = 5;
+                        name="Undead Foot Soldier";
+                        niceHair=true;
+                    }
+                    else if(monsters==1)
+                    {
+                        health=28;
+                        strength = 9;
+                        defense = 8;
+                        speed = 5;
                         name="Hell GateKeeper";
                         niceHair=true;
                     }
